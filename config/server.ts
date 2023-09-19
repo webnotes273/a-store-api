ce4cc55ac85dbd9a923098454ae58ff9fde58822
dev <email.box273@gmail.com>
2023-09-19 17:31:23 +0300

    update schema

--- config/server.ts
+++ config/server.ts
@@ -2,6 +2,7 @@ export default ({ env }) => ({
   host: env('HOST', 'localhost'),
   port: env.int('PORT', 1337),
   url: env('API_URL', 'http://localhost:1337'),
+  proxy: true,
   app: {
     keys: env.array('APP_KEYS'),
   },
